36836547570a811a65bdf6ff61313444190ee3a4
Ahmed <aahmedsamy.as@gmail.com>
2023-10-01 08:41:26 +0300

    limit test user to be read only

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,15 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 const {
-  createJob,
-  deleteJob,
-  getAllJobs,
-  updateJob,
-  getJob,
+    createJob,
+    deleteJob,
+    getAllJobs,
+    updateJob,
+    getJob,
 } = require('../controllers/jobs')
+const testUser = require("../middleware/testUser");
 
-router.route('/').post(createJob).get(getAllJobs)
+router.route('/').post(testUser, createJob).get(getAllJobs)
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
 
 module.exports = router
