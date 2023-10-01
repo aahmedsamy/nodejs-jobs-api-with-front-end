ac16580665ac5268526e1d90c0ea9f6549b7e2e9
Ahmed <aahmedsamy.as@gmail.com>
2023-10-01 06:20:04 +0300

    add paths/routes to handle frontend

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 require('express-async-errors');
-
+const path = require('path')
 // extra security packages
 const helmet = require('helmet');
 const xss = require('xss-clean');
-
 
 
 const express = require('express');
@@ -19,8 +18,11 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+app.use(express.static(path.resolve(__dirname, "./client/build")))
 app.use(express.json());
 app.use(helmet());
+
 app.use(xss());
 
 
@@ -28,20 +30,24 @@ app.use(xss());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
+// serve index.html
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
